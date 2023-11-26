a8b6088bfafa3d9794f698a0ad476be95cf1b10e
Juan <josemargri3@gmail.com>
2023-11-25 19:34:23 -0500

    Corrigiendo la estructura del repositorio

--- RPC_25_11_23/prueba.py
+++
@@ -1,9 +0,0 @@
-def obtener_numeros(n):
-    limite = (n - 1) // 2
-    numeros = [2 * i + 1 for i in range(limite + 1)]
-    return numeros
-
-# Ejemplo
-n = 10
-resultado = obtener_numeros(n)
-print(resultado)
